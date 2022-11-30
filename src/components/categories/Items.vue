f7be41266cc05f3cc6543f9309188d0783dc460b
ayuramadhann <ayu.ramadhan@ejen2u.com>
2022-11-29 23:16:04 -0800

    integrate category items

---
+++ src/components/categories/Items.vue
@@ -0,0 +1,48 @@
+<script setup>
+import {ref, onMounted} from 'vue'
+import { useRoute } from 'vue-router';
+import axios from 'axios'
+
+import ItemsCard from './../ItemsCard.vue'
+
+const items = ref([])
+const category = ref([])
+const route = useRoute()
+
+
+async function getItemsData (){
+  try {
+    const response = await axios.get('http://zullkit-backend.buildwithangga.id/api/categories?id='+ route.params.id +'&show_product=1')
+    items.value = response.data.data.products
+    category.value = response.data.data
+    console.log(response.data.data)
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+onMounted(() => {
+  getItemsData()
+})
+// const items = ref([
+//   {id: 1, title: 'Mobile UI Kit', description: 'Mobile UI Kit', image: 'categories-1.jpg'},
+//   {id: 2, title: 'Fonts', description: 'Mobile UI Kit', image: 'categories-2.jpg'},
+//   {id: 3, title: 'Icon Set', description: 'Mobile UI Kit', image: 'categories-3.jpg'},
+// ])
+
+</script>
+<template>
+<div class="container px-4 mx-auto my-16 md:px-12">
+      <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">Category > {{ category.name }}</h2>
+      <div class="flex flex-wrap -mx-1 lg:-mx-4">
+        <ItemsCard
+        v-for="item in items"
+          :id = item.id
+          :key = item.id
+          :title= item.name
+          :description= item.subtitle
+          :image= item.thumbnails
+        />
+      </div>
+    </div>
+</template> 
